1de9120bb9ff770f08349462a197f0b1a3648525
fakeshadow <24548779@qq.com>
2020-04-16 22:20:43 +0800

    add locale choice

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "manifest.json": "a61d1c58343dddcefd141e0224722130",
-"main.dart.js": "7b4c1c8f7606208b8e17bd40e910d949",
+"main.dart.js": "03e37ae17ce9f9625418b14c5f860a1f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
